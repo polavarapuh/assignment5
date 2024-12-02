b76f4fb6dc7caf7a765d35728a1cd406f87f2c24
polavarapuh <polavarapuharikrishna0516@gmail.com>
2024-12-02 03:03:07 -0600

    Update WeatherContext.js

--- src/WeatherContext.js
+++ src/WeatherContext.js
@@ -11,7 +11,7 @@ export const WeatherProvider = ({ children }) => {
     setLoading(true);
     setError("");
     try {
-      const API_KEY = "908c64104addee5b646a27f0069039b4";
+      const API_KEY = "paste your api key"; /* for ex: 908c64104addee5b646a27f0069039b4*/
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
       );
